c3c41dd143dc4871b3b07ed6cac7e707c8195787
ajn404 <nhy1423238345@163.com>
2025-03-14 14:08:10 +0800

    新增卡车组件，更新地图逻辑以支持卡车生成

--- code/my-crossy-road-game/src/components/Map.js
+++ code/my-crossy-road-game/src/components/Map.js
@@ -3,21 +3,53 @@ import { Grass } from "./Grass";
 import { Road } from "./Road";
 import { Tree } from "./Tree";
 import { Car } from "./Car";
+import { Truck } from "./Truck";
 
 export const metadata = [
   {
     type: "car",
     direction: false,
-    speed: 1,
-    vehicles: [{ initialTileIndex: 2, color: 0xff0000 }],
+    speed: 188,
+    vehicles: [
+      { initialTileIndex: -4, color: 0xbdb638 },
+      { initialTileIndex: -1, color: 0x78b14b },
+      { initialTileIndex: 4, color: 0xa52523 },
+    ],
+  },
+  {
+    type: "forest",
+    trees: [
+      { tileIndex: -5, height: 50 },
+      { tileIndex: 0, height: 30 },
+      { tileIndex: 3, height: 50 },
+    ],
+  },
+  {
+    type: "truck",
+    direction: true,
+    speed: 125,
+    vehicles: [
+      { initialTileIndex: -4, color: 0x78b14b },
+      { initialTileIndex: 0, color: 0xbdb638 },
+    ],
+  },
+  {
+    type: "forest",
+    trees: [
+      { tileIndex: -8, height: 30 },
+      { tileIndex: -3, height: 50 },
+      { tileIndex: 2, height: 30 },
+    ],
   },
 ];
 
 export const map = new THREE.Group();
 
 export function initializeMap() {
-  const grass = Grass(0);
-  map.add(grass);
+  for (let rowIndex = 0; rowIndex > -5; rowIndex--) {
+    const grass = Grass(rowIndex);
+    map.add(grass);
+  }
   addRows();
 }
 
@@ -46,6 +78,21 @@ export function addRows() {
           vehicle.color
         );
         row.add(car);
+      });
+
+      map.add(row);
+    }
+
+    if (rowData.type === "truck") {
+      const row = Road(rowIndex);
+
+      rowData.vehicles.forEach((vehicle) => {
+        const truck = Truck(
+          vehicle.initialTileIndex,
+          rowData.direction,
+          vehicle.color
+        );
+        row.add(truck);
       });
 
       map.add(row);
